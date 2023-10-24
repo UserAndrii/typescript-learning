96f1f5d321442ba63dbab301b2296d3b5bdaabb5
Andrii Gadar <gadar.andre@gmail.com>
2023-10-24 23:15:26 +0300

    solving JS problems, training

--- index.js
+++ index.js
@@ -103,3 +103,101 @@
 // console.log(incrementString("fo99obar99"));
 
 /**-------------------------------------------------------------------------------------------------------------------------- */
+
+// function scramble(str1, str2) {
+//   const count = (str) =>
+//     str.split("").reduce((acc, char) => {
+//       acc[char] = (acc[char] || 0) + 1;
+//       return acc;
+//     }, {});
+
+//   return Object.keys(count(str2)).every(
+//     (char) => count(str2)[char] <= (count(str1)[char] || 0)
+//   );
+// }
+
+// console.log(scramble("rkqodlw", "world")); // ==> True
+// console.log(scramble("cedewaraaossoqqyt", "codewars")); // ==> True
+// console.log(scramble("katas", "steak")); // ==> False
+// console.log(scramble("scriptjavx", "javascript")); // ==> False
+
+/**-------------------------------------------------------------------------------------------------------------------------- */
+
+// const a = {
+//   John: "aye",
+//   Michael: "aye",
+//   Ivan: "aye",
+//   Joe: "aye",
+// };
+
+// const b = {
+//   John: "aye",
+//   Michael: "aye",
+//   Ivan: "aye",
+//   Joe: "nay",
+// };
+
+// const isFire = (obj) => {
+//   if (Object.values(obj).includes("nay")) {
+//     return console.log("We are not ready!");
+//   }
+//   console.log("Fire!");
+// };
+
+// isFire(a);
+// isFire(b);
+
+/**----------------------------------------------------------------------------------------------------------------------------------- */
+
+// const array = [1, 2, 3, 4, 5, 6, 7, 8, 10];
+// const result = array.filter((el) => el % 2 === 0);
+// console.log(result);
+
+/**----------------------------------------------------------------------------------------------------------------------------------- */
+
+// function add(a, b) {
+//   return (BigInt(a) + BigInt(b)).toString();
+// }
+
+// function add(a, b) {
+//   let result = "";
+//   let carry = 0;
+//   const maxLength = Math.max(a.length, b.length);
+
+//   for (let i = 0; i < maxLength; i++) {
+//     const numA = parseInt(a[a.length - 1 - i]) || 0;
+//     const numB = parseInt(b[b.length - 1 - i]) || 0;
+
+//     let sum = numA + numB + carry;
+//     carry = Math.floor(sum / 10);
+//     result = (sum % 10).toString() + result;
+//   }
+
+//   if (carry > 0) {
+//     result = carry.toString() + result;
+//   }
+//   return result;
+// }
+
+// console.log(add("1", "1")); // "2"
+// console.log(add("123", "456")); // "579"
+// console.log(add("888", "222")); // "1110"
+// console.log(add("1372", "69")); // "1441"
+// console.log(add("12", "456")); // "468"
+// console.log(add("101", "100")); // "201"
+// console.log(add("63829983432984289347293874", "90938498237058927340892374089")); // "91002328220491911630239667963"
+
+/**----------------------------------------------------------------------------------------------------------------------------------- */
+
+// const REGEXP = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[A-Za-z\d]{6,}$/;
+
+/**
+ * ^: Початок рядка.
+ * (?=.*[a-z]): - у рядку повинна бути принаймні одна мала літера.
+ * (?=.*[A-Z]):  - у рядку повинна бути принаймні одна велика літера.
+ * (?=.*\d):  - у рядку повинна бути принаймні одна цифра.
+ * [a-zA-Z\d_]{6,}: Послідовність букв, цифр та підкреслень довжиною принаймні 6 символів.
+ * $: Кінець рядка.
+ */
+
+/**----------------------------------------------------------------------------------------------------------------------------------- */
